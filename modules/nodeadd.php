fcc13dd8332c8e81afc445a4c1d386c2946c257f
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2017-10-30 11:03:13 +0100

    bugfix: when we add new node with whole network checked, we should be redirected to netinfo (fixes LMS #1201)

--- modules/nodeadd.php
+++ modules/nodeadd.php
@@ -250,9 +250,11 @@ if (isset($_POST['nodedata']))
 		);
 		$nodedata = $hook_data['nodeadd'];
 
-		if (!isset($nodedata['reuse'])) {
-			$SESSION->redirect('?m=nodeinfo&id='.$nodeid);
-		}
+		if (!isset($nodedata['reuse']))
+			if (isset($nodedata['wholenetwork']))
+				$SESSION->redirect('?m=netinfo&id=' . $nodedata['netid']);
+			else
+				$SESSION->redirect('?m=nodeinfo&id=' . $nodeid);
 
 		$ownerid = $nodedata['ownerid'];
 		unset($nodedata);
