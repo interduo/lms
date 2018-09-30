0203b840f8f00079bccd9f72eee21f48ce16a861
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2018-09-30 15:01:46 +0200

    improvement: tariffs are not unique per promotion schema

--- lib/LMSDB_common.class.php
+++ lib/LMSDB_common.class.php
@@ -24,7 +24,7 @@
  *  $Id$
  */
 
-define('DBVERSION', '2018022603'); // here should be always the newest version of database!
+define('DBVERSION', '2018022604'); // here should be always the newest version of database!
 				 // it placed here to avoid read disk every time when we call this file.
 
 /**
