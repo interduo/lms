75494e69a0bbd169d9e6de436be7a4ee2dc8a543
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2019-04-29 17:18:17 +0200

    bugfix: fixed invoice note generation in some corner cases

--- modules/invoicenote.php
+++ modules/invoicenote.php
@@ -272,8 +272,8 @@ switch($action)
 				$contents[$idx]['cash'] = f_round(-1 * ($contents[$idx]['valuebrutto'] - $invoicecontents[$idx]['valuebrutto']), 2);
 				$contents[$idx]['valuebrutto'] = f_round(($contents[$idx]['valuebrutto'] - $invoicecontents[$idx]['valuebrutto']) / $contents[$idx]['count']);
 				$contents[$idx]['count'] = 0;
-			} elseif (($contents[$idx]['valuenetto'] != $item[$idx]['valuenetto'])
-					|| ($contents[$idx]['valuebrutto'] != $item[$idx]['valuebrutto'])) {
+			} elseif (($contents[$idx]['valuenetto'] != $item['valuenetto'])
+					|| ($contents[$idx]['valuebrutto'] != $item['valuebrutto'])) {
 				$contents[$idx]['cash'] = f_round(-1 * ($contents[$idx]['valuebrutto'] * $contents[$idx]['count']
 					- $invoicecontents[$idx]['valuebrutto'] * $invoicecontents[$idx]['count']), 2);
 				$contents[$idx]['valuebrutto'] = f_round($contents[$idx]['valuebrutto'] - $invoicecontents[$idx]['valuebrutto']);
