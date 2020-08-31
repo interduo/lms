6200a7a9962fb5124ce150e1677e91c1bebf7d7d
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2020-08-31 14:37:18 +0200

    improvement: allow to use assignments with backward period accounting (promotion schema support - LMS+ #1425)

--- lib/upgradedb/mysql.2020050406.php
+++ lib/upgradedb/mysql.2020050406.php
@@ -25,6 +25,6 @@ $this->BeginTrans();
 
 $this->Execute("ALTER TABLE promotionassignments ADD COLUMN backwardperiod smallint DEFAULT 0 NOT NULL");
 
-$this->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2020083100', 'dbversion'));
+$this->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2020050406', 'dbversion'));
 
 $this->CommitTrans();
