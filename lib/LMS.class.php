1c22ca5c17deca15b5519d8a4387389083b9d4e5
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2020-12-10 09:52:16 +0100

    devel: released stable 25.24

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -30,7 +30,7 @@
 class LMS
 {
     const SOFTWARE_NAME = 'LMS';
-    const SOFTWARE_VERSION = '25.23';
+    const SOFTWARE_VERSION = '25.24';
     const SOFTWARE_REVISION = '$Format:%cI$'; // %H for last commit checksum
 
     public $DB;   // database object
