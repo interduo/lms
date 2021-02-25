d2a4ab5a4c5a3c871000d8c7d0f54004defee9c7
Rafał Pietraszewicz <r.pietraszewicz@gmail.com>
2021-02-25 13:46:40 +0100

    enhancement: allow to store mac address in net device (LMS+ #1591) (#1962)

--- lib/LMSDB_common.class.php
+++ lib/LMSDB_common.class.php
@@ -24,7 +24,7 @@
  *  $Id$
  */
 
-define('DBVERSION', '2020121402'); // here should be always the newest version of database!
+define('DBVERSION', '2020121403'); // here should be always the newest version of database!
 
 /**
  *
