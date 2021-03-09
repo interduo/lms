8a89a1b8866f13c05e08526cc68527015536fad2
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-03-09 11:50:48 +0100

    bugfix: fixed mobile phone detection

--- modules/rtticketadd.php
+++ modules/rtticketadd.php
@@ -251,7 +251,7 @@ if (isset($_POST['ticket'])) {
                         return $contact['fullname'];
                 }, $all_phones);
                 $mobile_phones = array_filter($all_phones, function ($contact) {
-                    return $contact['type'] & (CONTACT_MOBILE | CONTACT_DISABLED) == CONTACT_MOBILE;
+                    return ($contact['type'] & (CONTACT_MOBILE | CONTACT_DISABLED)) == CONTACT_MOBILE;
                 });
 
                 if (ConfigHelper::checkConfig('phpui.helpdesk_customerinfo')) {
