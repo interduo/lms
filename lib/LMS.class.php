e1a8e2f37f833906a7942dd62da24b8f052214d1
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-04-29 10:47:30 +0200

    devel/improvement: LMS::SOFTWARE_URL stored URL of LMS home page

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -31,6 +31,7 @@ class LMS
 {
     const SOFTWARE_NAME = 'LMS';
     const SOFTWARE_VERSION = '26.22';
+    const SOFTWARE_URL = 'https://lms-plus.org';
     const SOFTWARE_REVISION = '$Format:%cI$'; // %H for last commit checksum
 
     public $DB;   // database object
