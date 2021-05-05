73c77fe3fdbb5040f2eb17b54aae62281192299a
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-05-05 12:14:58 +0200

    improvement/performance: document post address update should not use address record deletions as it can have meaningfull impact on bulk update operation made in DB transactions

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -3313,6 +3313,12 @@ class LMS
         return $manager->UpdateAddress($args);
     }
 
+    public function SetAddress($args)
+    {
+        $manager = $this->getLocationManager();
+        return $manager->SetAddress($args);
+    }
+
     public function UpdateCustomerAddress($customer_id, $args)
     {
         $manager = $this->getLocationManager();
