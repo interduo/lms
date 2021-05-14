84e7769a5eafaa5609d6103fa8350c05489b8cb2
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-05-14 09:18:25 +0200

    enhancement: allow to mark division as natural person - birth date support added

--- lib/LMSDB_common.class.php
+++ lib/LMSDB_common.class.php
@@ -24,7 +24,7 @@
  *  $Id$
  */
 
-define('DBVERSION', '2020121410'); // here should be always the newest version of database!
+define('DBVERSION', '2020121411'); // here should be always the newest version of database!
 
 /**
  *
