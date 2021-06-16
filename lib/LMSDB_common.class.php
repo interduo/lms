f22f4294449525b0033651ce1c0c3666cc34c319
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-06-16 15:44:08 +0200

    bugfix: db schema couldn't store percentage discount equal 100.00

--- lib/LMSDB_common.class.php
+++ lib/LMSDB_common.class.php
@@ -24,7 +24,7 @@
  *  $Id$
  */
 
-define('DBVERSION', '2020121412'); // here should be always the newest version of database!
+define('DBVERSION', '2020121413'); // here should be always the newest version of database!
 
 /**
  *
