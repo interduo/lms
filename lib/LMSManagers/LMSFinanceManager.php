26d40570740bea962d5c2f19b1efb36468b210dc
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-06-24 16:34:56 +0200

    bugfix: fixed all assignment suspension support (LMS+ fixes #1869)

--- lib/LMSManagers/LMSFinanceManager.php
+++ lib/LMSManagers/LMSFinanceManager.php
@@ -1006,7 +1006,7 @@ class LMSFinanceManager extends LMSManager implements LMSFinanceManagerInterface
 
             if ($data['datefrom'] < $data['dateto'] || !$data['datefrom'] || !$data['dateto']) {
                 // creates one assignment record
-                if (empty($data['tariffid'])) {
+                if (isset($data['tariffid']) && empty($data['tariffid'])) {
                     $args = array(
                         'name' => $data['name'],
                         'value' => str_replace(',', '.', $data['value']),
