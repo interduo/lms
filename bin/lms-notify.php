5db9613d45188fe0b0356fd0bca660956530941b
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-07-29 13:32:25 +0200

    bugfix: fixed assignment-invoice action prechecks

--- bin/lms-notify.php
+++ bin/lms-notify.php
@@ -2752,7 +2752,7 @@ if (!empty($intersect)) {
                                         break;
                                 }
                                 $where[] = 'EXISTS (SELECT id FROM assignments
-                                    WHERE invoice = ' . $target_doctype . ' AND (tariffid IS NOT NULL OR liabilityid IS NOT NULL)
+                                    WHERE invoice <> ' . $target_doctype . ' AND (tariffid IS NOT NULL OR liabilityid IS NOT NULL)
                                         AND datefrom <= ?NOW? AND (dateto = 0 OR dateto >= ?NOW?)
                                         AND customerid = c.id)';
                                 break;
@@ -3070,7 +3070,7 @@ if (!empty($intersect)) {
 
                                         if (!$debug) {
                                             $DB->Execute("UPDATE assignments SET invoice = ?
-                                                WHERE id = ?", array(DOC_INVOICE, $assign['id']));
+                                                WHERE id = ?", array($target_doctype, $assign['id']));
                                             if ($SYSLOG) {
                                                 $SYSLOG->NewTransaction('lms-notify.php');
                                                 $SYSLOG->AddMessage(
