4aa81ae315e5ac22b0225005a127d94833d59e8f
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-09-09 17:58:23 +0200

    devel: released 27.2

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -30,7 +30,7 @@
 class LMS
 {
     const SOFTWARE_NAME = 'LMS+';
-    const SOFTWARE_VERSION = '27.1';
+    const SOFTWARE_VERSION = '27.2';
     const SOFTWARE_URL = 'https://lms-plus.org';
     const SOFTWARE_REVISION = '$Format:%cI$'; // %H for last commit checksum
 
