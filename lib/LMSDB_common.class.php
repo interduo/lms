3d7e51dceca70a40e4f5cb86c28828a599f85a95
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-12-15 13:41:59 +0100

    improvement: better performance of vinvoiceview (especially on mysql/mariadb) (LMS+ #2019) - special thanks go to @bestgo

--- lib/LMSDB_common.class.php
+++ lib/LMSDB_common.class.php
@@ -24,7 +24,7 @@
  *  $Id$
  */
 
-define('DBVERSION', '2021072905'); // here should be always the newest version of database!
+define('DBVERSION', '2021072906'); // here should be always the newest version of database!
 
 /**
  *
