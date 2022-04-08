f947c13ae534c9bec57b8436977b407ee06ee76c
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-04-08 11:50:28 +0200

    improvement/bugfix: fixed documentation url to use selected ui locale and allow to easily change this url (LMS+ fixes #2110)

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -32,6 +32,7 @@ class LMS
     const SOFTWARE_NAME = 'LMS+';
     const SOFTWARE_VERSION = '27.25';
     const SOFTWARE_URL = 'https://lms-plus.org';
+    const SOFTWARE_DOCUMENTATION_URL = 'https://wiki.lms-plus.org';
     const SOFTWARE_REVISION = '$Format:%cI$'; // %H for last commit checksum
 
     public $DB;   // database object
