b5afbc25906c6e3d2a3f85ca6647e4b4f46bdc97
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-06-13 17:33:58 +0200

    enhancement: toggle view of documents related to invoices

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -3501,6 +3501,12 @@ class LMS
         return $manager->isDocumentAccessible($docid);
     }
 
+    public function getDocumentReferences($docid)
+    {
+        $manager = $this->getDocumentManager();
+        return $manager->getDocumentReferences($docid);
+    }
+
     /*
      *  Location
      */
