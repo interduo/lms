87335a51637bf54954dc2641978012e56e88207d
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-07-15 18:21:16 +0200

    devel: released 27.33

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -30,7 +30,7 @@
 class LMS
 {
     const SOFTWARE_NAME = 'LMS+';
-    const SOFTWARE_VERSION = '27.32';
+    const SOFTWARE_VERSION = '27.33';
     const SOFTWARE_URL = 'https://lms-plus.org';
     const SOFTWARE_DOCUMENTATION_URL = 'https://wiki.lms-plus.org';
     const SOFTWARE_REPO_URL = 'https://git.lms-plus.org';
