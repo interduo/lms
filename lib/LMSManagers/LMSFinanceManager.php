ecbea72d9bc5c9b181d779be00c930892b178543
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-07-28 10:56:59 +0200

    bugfix: fixed php warning

--- lib/LMSManagers/LMSFinanceManager.php
+++ lib/LMSManagers/LMSFinanceManager.php
@@ -628,7 +628,7 @@ class LMSFinanceManager extends LMSManager implements LMSFinanceManagerInterface
                     $at        = (ConfigHelper::checkValue(ConfigHelper::getConfig('phpui.promotion_preserve_at_day', true)) && $data['at'] !== '')
                                                ? $data['at'] : $this->CalcAt($period, $datefrom);
 
-                    $length    = $data_schema[$idx - 1];
+                    $length    = isset($data_schema[$idx - 1]) ? $data_schema[$idx - 1] : null;
                     $month     = date('n', $datefrom);
                     $year      = date('Y', $datefrom);
 
