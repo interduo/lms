3298da2dfac1d537b5a400176fda698c13fa0080
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-08-26 14:56:03 +0200

    improvement: added 'subtype' field to 'voip_cdr' table - it allows to mark cdr records in many different ways

--- lib/LMSDB_common.class.php
+++ lib/LMSDB_common.class.php
@@ -24,7 +24,7 @@
  *  $Id$
  */
 
-define('DBVERSION', '2021072916'); // here should be always the newest version of database!
+define('DBVERSION', '2021072917'); // here should be always the newest version of database!
 
 /**
  *
