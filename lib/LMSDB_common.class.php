3f55855aa12a22c38fe88d384fd9c2eb19b71ec4
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-09-15 15:57:43 +0200

    devel/cleanup: renamed 'phpui.document_password' and 'phpui.document_protection_comand' to 'documents.protection_password' and 'documents.protection_command'

--- lib/LMSDB_common.class.php
+++ lib/LMSDB_common.class.php
@@ -24,7 +24,7 @@
  *  $Id$
  */
 
-define('DBVERSION', '2021072919'); // here should be always the newest version of database!
+define('DBVERSION', '2021072920'); // here should be always the newest version of database!
 
 /**
  *
