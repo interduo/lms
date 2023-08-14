1166b740738d233de00a75203588256589bc597f
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2023-08-14 12:46:26 +0200

    improvement: allow to filter by major technology of existing node

--- modules/netranges.php
+++ modules/netranges.php
@@ -438,10 +438,13 @@ function getBuildings(array $filter)
             ) : '')
         );
     }
+    $majortechs = isset($filter['majortechs']) && is_array($filter['majortechs']) ?
+        Utils::filterIntegers(array_values($filter['majortechs'])) : array();
+
     if (empty($buildings)) {
         $buildings = array();
     } else {
-        foreach ($buildings as &$building) {
+        foreach ($buildings as $key => &$building) {
             $city_id = intval($building['city_id']);
             $street_id = intval($building['street_id']);
             $street_ident = intval($building['street_ident']);
@@ -457,7 +460,13 @@ function getBuildings(array $filter)
                 $building['customers'] = array();
                 $building['existing'] = 0;
             }
-        }
+            if (!empty($majortechs)
+                && (!isset($nodes[$city_id][$street_ident][$building_num])
+                || !array_intersect($majortechs, array_keys($nodes[$city_id][$street_ident][$building_num]['linktechnologies'])))
+            ) {
+                unset($buildings[$key]);
+            }
+	    }
         unset($building);
         if (!empty($existing)) {
             $buildings = array_filter($buildings, function ($building) use ($existing) {
@@ -828,6 +837,7 @@ $SMARTY->assign('pagination', $pagination);
 $SMARTY->assign(array(
     'filter' => $filter,
     'linktechnologies' => $SIDUSIS_LINKTECHNOLOGIES,
+    'linktechnologiesall' => $LINKTECHNOLOGIES,
     'linkspeeds' => $linkspeeds,
     'range'=> $range,
     'unique_total' => $unique_total,
