b0695b1e1c46cff903de745746d747c26fed862d
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2026-08-27 08:26:21 +0200

    bugfix: dont present empty bracket when thre is no producer and model in netdevedit

--- modules/netdevedit.php
+++ modules/netdevedit.php
@@ -946,17 +946,16 @@ if (isset($netdev)) {
 } else {
     $netdev = $LMS->GetNetDev($id);
 
-    if (isset($netdev['producer'])) {
-        $netdev['pagetitle'] = trans('Device Edit: $a ($b)', $netdev['name'], $netdev['producer'] . ($netdev['model'] ? ' ' . $netdev['model'] : ''));
-    } else {
-        $netdev['pagetitle'] = trans('Device Edit: $a', $netdev['name']);
-    }
+    $producermodel = trim(($netdev['producer'] ?? '') . ' ' . ($netdev['model'] ?? ''));
+    $netdev['pagetitle'] = $producermodel ?
+        trans('Device Edit: $a ($b)', $netdev['name'], $producerModel) :
+        trans('Device Edit: $a', $netdev['name']);
 
-    if (isset($netdev['producerid']) && preg_match('/^[0-9]+$/', $netdev['producerid'])
-        && isset($netdev['modelid']) && preg_match('/^[0-9]+$/', $netdev['modelid'])) {
-        $netdev['producer'] = $netdev['producerid'];
-        $netdev['model'] = $netdev['modelid'];
-    }
+    $producermmodel = trim(str_replace('-', '', ($netdev['producer'] ?? '') . ' ' . ($netdev['model'] ?? '')));
+
+    $netdev['pagetitle'] = $producerModel 
+        ? trans('Device Edit: $a ($b)', $netdev['name'], $producerModel)
+        : trans('Device Edit: $a', $netdev['name']);
 
     $attachmenttype = 'netdevid';
     $attachmentresourceid = $id;
